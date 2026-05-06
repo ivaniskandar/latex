0538f4316e81139a30861f5ee875cac9502b17e5
Ivan Iskandar <iiiiskandar14@gmail.com>
2026-05-06 20:24:07 +0700

    git versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,3 +12,11 @@ plugins {
     alias(libs.plugins.kotlinxBenchmark) apply false
     alias(libs.plugins.mavenPublish) apply false
 }
+
+val gitShortHash: String = providers.exec {
+    commandLine("git", "rev-parse", "--short", "HEAD")
+}.standardOutput.asText.get().trim()
+
+allprojects {
+    extra.set("VERSION", gitShortHash)
+}
